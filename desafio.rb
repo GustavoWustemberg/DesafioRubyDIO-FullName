1684370317e99155eefdaffd0715951994417578
GustavoWustemberg <gustavowustemberg14@gmail.com>
2023-05-16 12:06:25 -0300

    Adjust the response

--- desafio.rb
+++ desafio.rb
@@ -2,8 +2,8 @@
 print "Insert your name: "
 name = gets.chomp.capitalize
 print "Insert your surname: "
-surname = gets.chomp.capitalize
+lastname = gets.chomp.capitalize
 print "Insert your age: "
 age = gets.chomp.to_i
 # Response
-puts "#{name} #{surname} #{age} years old"
+puts "Hello #{name} #{lastname}! Welcome! Your age is #{age}.
