bf1c35f380f0a35cbdd324638c34b960c2f4a451
GustavoWustemberg <gustavowustemberg14@gmail.com>
2023-05-16 12:08:51 -0300

    Alter image

--- desafio.rb
+++ desafio.rb
@@ -6,4 +6,4 @@ lastname = gets.chomp.capitalize
 print "Insert your age: "
 age = gets.chomp.to_i
 # Response
-puts "Hello #{name} #{lastname}! Welcome! Your age is #{age}.
+puts "Hello #{name} #{lastname}! Welcome! Your age is #{age}."
